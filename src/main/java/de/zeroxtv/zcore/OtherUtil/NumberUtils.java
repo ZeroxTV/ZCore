4b330823d3d7bc5d087b4bc7c58fe0944c7afd97
ZeroxTV <leonbaronick@gmail.com>
2017-01-19 19:32:52 +0100

    Added NumberUtils to format doubles

---
+++ src/main/java/de/zeroxtv/zcore/OtherUtil/NumberUtils.java
@@ -0,0 +1,17 @@
+package de.zeroxtv.zcore.OtherUtil;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by ZeroxTV
+ */
+public class NumberUtils {
+    public double parseDouble(double toParse, int decimals) {
+        String pattern = "#.";
+        for (int i = 0; i < decimals; i++) {
+            pattern = pattern + "#";
+        }
+        DecimalFormat df = new DecimalFormat(pattern);
+        return Double.valueOf(df.format(toParse));
+    }
+}
